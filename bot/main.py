ba49d41f44de7d3718c9e56625689b632d17137b
shavkatJuraboyev <shavkatjoraboyev0@gmail.com>
2025-05-01 16:09:41 +0500

    yangilandi'

--- bot/main.py
+++ bot/main.py
@@ -7,7 +7,7 @@ from handlers.user_handlers import register_user_handlers
 from utils.auth import is_admin
 
 # Bot tokenini o'rnatish
-BOT_TOKEN = "7737434094:AAGwgKgxHii0XCoQgyb4okysOFwQzwpgeIc"
+BOT_TOKEN = "6299868793:AAF4HaBzPZAFuwMznn6RJFnvfsrr8q-Ergw"
 
 async def main():
     # Bot va Dispatcher obyektlarini yaratish
