ad3e2adcc57fbb169370a420827a293f2b86bfdf
shavkatJuraboyev <shavkatjoraboyev0@gmail.com>
2026-01-06 12:14:31 +0500

    'yangilandi'

--- bot/main.py
+++ bot/main.py
@@ -3,7 +3,7 @@ import pytz
 from aiogram import Bot, Dispatcher
 from database.db import init_db
 from aiogram.fsm.storage.memory import MemoryStorage
-from handlers.admin_handlers import register_admin_handlers
+from handlers.admin_handlers import register_admin_handlers, send_birthday_notifications, obhavo_command_telegram
 from handlers.user_handlers import register_user_handlers
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from aiogram.client.default import DefaultBotProperties
@@ -19,8 +19,8 @@ async def main():
     register_user_handlers(dp, bot)
 
     scheduler = AsyncIOScheduler(timezone=pytz.timezone("Asia/Tashkent"))
-    # scheduler.add_job(send_birthday_notifications, trigger='cron', hour=8, minute=00, misfire_grace_time=20)
-    # scheduler.add_job(obhavo_command_telegram, trigger='cron', hour=6, minute=0, misfire_grace_time=30)
+    scheduler.add_job(send_birthday_notifications, trigger='cron', hour=8, minute=00, misfire_grace_time=20)
+    scheduler.add_job(obhavo_command_telegram, trigger='cron', hour=6, minute=0, misfire_grace_time=30)
     scheduler.start()
     try:
         await dp.start_polling(bot)
